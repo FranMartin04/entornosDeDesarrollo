3f3577b73b1b63bc255b0198c9bcb86bfa4174b3
Usuario <Usuario@192.168.12.219>
2023-09-29 14:28:57 +0200

    Feat(Added) My name is

--- src/es/marquesdecomares/daw/ed/HolaMundo.java
+++ src/es/marquesdecomares/daw/ed/HolaMundo.java
@@ -3,9 +3,9 @@ package es.marquesdecomares.daw.ed;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
-			System.out.println("Universo");
-			System.out.println("Hola");
+		System.out.println("Me llamo Alejandro");
+			System.out.println("Pon tu nombre");
+			System.out.println("Pon tu nombre");
 			
 	}
 
